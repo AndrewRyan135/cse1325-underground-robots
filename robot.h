d9a37e13b7b8f31cbf08dd799330bfda93171d77
warren@smith4.net <warren@smith4.net>
2017-03-25 22:26:58 -0500

    Added robot parts

---
+++ robot.h
@@ -0,0 +1,51 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+#include <string>
+using namespace std;
+class Robot_part{
+protected:
+    string name;
+    int model_number;
+    double cost;
+    string description;
+    string image_filename;
+public:
+    Robot_part();
+
+class Head : public Robot_part{
+private:
+    double power;
+public:
+    Head();
+};
+
+class Torso : public Robot_part{
+private:
+    int battery_compartments;
+    int max_arms;
+public:
+    Torso();
+};
+
+class Arm : public Robot_part{
+private:
+    double max_power;
+public:
+    Arm();
+};
+
+class Locomotor : public Robot_part{
+private:
+    double max_power;
+public:
+    Locomotor();
+};
+
+class Battery : public Robot_part{
+private:
+    double power_availible;
+    double max_energy;
+public:
+    Battery();
+};
+#endif
