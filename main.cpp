b250cc2e3c5713fe4cd2bf43a80a2a9bd208dffb
Andrew <Andrew>
2017-03-27 15:08:22 -0500

    Added functionallity to the main file and created error handeling

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
-#include "catologe.h"
+#include <limits>
+#include "catalog.h"
 #include "customer.h"
 #include "robot.h"
 using namespace std;
 
-Catologe catologe;
+Catalog catologe;
 
 void create_part();
 
@@ -25,21 +26,26 @@ class View
 		   << "\n1. Customer"
 		   << "\n2. Sales Associate"
 		   << "\n3. Product Manager"
-		   << "\n4. Boss";
+		   << "\n4. Boss"
+		   << "\n5. Exit";
 		return os.str();
 	}
-	void menu_choice(int cmd)
-	{
-		switch(cmd) {
-			case 1 : cout << "Browse robot cataloge" << endl; break;
-			case 2 : cout << "To be finish in later sprint" << endl; break;
-			case 3 : cout << "1. Create robot part" << endl
-					      << "2. Create new robot model" << endl; break;
-			case 4 : cout << "1. Create new customer" << endl
-				          << "2. Create new sales associate" << endl; break;
-			default : cout << "Invalid Input" << endl; 
-		}
-	}
+	//void menu_choice(int cmd)
+	//{
+	//	switch(cmd) {
+	//		case 1 : cout << "Browse robot cataloge" << endl; 
+	//		break;
+	//		case 2 : cout << "To be finish in later sprint" << endl; 
+	//		break;
+	//		case 3 : cout << "1. Create robot part" << endl
+	//				      << "2. Create new robot model" << endl; 
+	//				      break;
+	//		case 4 : cout << "1. Create new customer" << endl
+	//			          << "2. Create new sales associate" << endl; 
+	//			          break;
+	//		default : cout << "Invalid Input" << endl; 
+	//	}
+	//}
 	string show_cataloge()
 	{
 		//print out the vector
@@ -53,19 +59,25 @@ class Controller
 		{
 			cin >> cmd;
 			switch(cmd) {
-			case 1 : //call to print out cataloge 
-			case 2 : 
-			case 3 : cin >> cmd;
+			case 1 :  cout << "Browse robot cataloge" << endl; 
+					  break;
+			case 2 :  cout << "To be finish in later sprint" << endl; 
+					  break;
+			case 3 : cout << "1. Create robot part" << endl
+					      << "2. Create new robot model" << endl; 
+					 cin >> cmd;
 				     if (cmd == 1)
 				     {
-				 	 	create_part();
+				     	create_part();
 				     }
 				     else if (cmd == 2)
 				     {
-				 	 	//new robot model
+				     	//create_model
 				     }
 				     break;
-			case 4 : cin >> cmd;
+			case 4 : cout << "1. Create new customer" << endl
+				          << "2. Create new sales associate" << endl;
+					 cin >> cmd;
 				     if (cmd == 1)
 				     {
 					     string name, phone_number, email;
@@ -80,14 +92,21 @@ class Controller
 					     cin >> email;
 					     
 					     Customer customer(name, number, phone_number, email);
-					     cout << customer.to_string() << endl;
-					     //catologe.add_customer(customer);
-				 	     //new customer
+					     catologe.add_customer(customer);
 				     }
 				     else if (cmd == 2)
 				     {
-				 	     //new sales associate
+				 	     string name;
+				 	     int employee_number;
+				 	     cout << "Enter the employee's name: " << endl;
+				 	     cin >> name;
+				 	     cout << "Enter the employee's employee number: " << endl;
+				 	     cin >> employee_number;
+
+				 	     SalesAssociate associate(name,employee_number);
 				     }
+				     break;
+			case 5 : exit(0);
 			default : {}
 			}
 	       }
@@ -98,20 +117,23 @@ class Controller
 
 int main()
 {
+	while (true)
+	{
 	int cmd = 0;
 	View view;
 	Controller controller;
 	cout << view.get_menu() << endl;
-	cin >> cmd;
-	view.menu_choice(cmd);
 	controller.menu_choice(cmd);
+	}
 }
 
 void create_part()
 {
 	int cmd, _model_number, _battery_compartments, _max_arms;
+	int flag = 1;
 	string _name, _description, _image_file;
 	double _cost, _power, _max_power, _power_available, _max_energy;
+	bool valid = false;
 	stringstream os;
 	os << "1. Head" << "\n"
 	   << "2. Torso" << "\n"
@@ -124,76 +146,281 @@ void create_part()
 		case 1 : {cout << "Enter the part name: " << endl;
 				 cin >> _name;
 				 cout << "Enter the model number: " << endl;
-				 cin >> _model_number;
+				 while (flag == 1)
+				 {
+				   cin >> _model_number;
+				   if (cin.fail())
+				   {
+				 	  cin.clear();
+				 	  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				 	  cout << "Invalid input: Please re enter: " << endl;
+				 	  continue;
+				   }
+				 flag = 0;
+				 }
+				 flag =1;
 				 cout << "Enter the cost of the part: " << endl;
-				 cin >> _cost;
+				 while (flag == 1)
+				 {
+				   cin >> _cost;
+				   if (cin.fail())
+				   {
+				    	cin.clear();
+				    	cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				    	cout << "Invalid input: Pease re enter: " << endl;
+				    	continue;
+				   }
+				 flag = 0;
+				 }
+				 flag = 1;
 				 cout << "Enter the part description: " << endl;
 				 cin >> _description;
-				 cout << "Enter the mage file name: " << endl;
+				 cout << "Enter the image file name: " << endl;
 				 cin >> _image_file;
 				 cout << "Enter the part power: " << endl;
-				 cin >> _power;
-				 Head stuff(_name,_model_number,_cost,_description,_image_file,_power);
+				 while (flag == 1)
+				 {
+				   cin >> _power;
+				   if (cin.fail())
+				   {
+				   		cin.clear();
+				    	cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				    	cout << "Invalid input: Pease re enter: " << endl;
+				    	continue;
+				   }
+				 flag = 0;
+				 }
+				 flag = 1;
+				 Head head(_name,_model_number,_cost,_description,_image_file,_power);
 				 break;}
 		case 2 : {cout << "Enter the part name: " << endl;
 				 cin >> _name;
 				 cout << "Enter the model number: " << endl;
-				 cin >> _model_number;
+				 while (flag == 1)
+				 {
+				   cin >> _model_number;
+				   if (cin.fail())
+				   {
+				 	  cin.clear();
+				 	  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				 	  cout << "Invalid input: Please re enter: " << endl;
+				 	  continue;
+				   }
+				 flag = 0;
+				 }
+				 flag =1;
 				 cout << "Enter the cost of the part: " << endl;
-				 cin >> _cost;
+				 while (flag == 1)
+				 {
+				   cin >> _cost;
+				   if (cin.fail())
+				   {
+				    	cin.clear();
+				    	cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				    	cout << "Invalid input: Pease re enter: " << endl;
+				    	continue;
+				   }
+				 flag = 0;
+				 }
+				 flag = 1;
 				 cout << "Enter the part description: " << endl;
 				 cin >> _description;
-				 cout << "Enter the mage file name: " << endl;
+				 cout << "Enter the image file name: " << endl;
 				 cin >> _image_file; 
 				 cout << "Enter the number of battery compartments: " << endl;
-				 cin >> _battery_compartments;
+				 while (flag == 1)
+				 {
+				   cin >> _battery_compartments;
+				   if (cin.fail())
+				   {
+				   		cin.clear();
+				   		cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				   		cout << "Invalid input: Please re enter: " <<endl;
+				   		continue;
+				   }
+				 flag = 0;
+				 }
+				 flag = 1;
 				 cout << "Enter the number of arms: " << endl;
-				 cin >> _max_arms;
-				 Torso stuff2(_name,_model_number,_cost,_description,_image_file,_battery_compartments,_max_arms);
+				 while (flag == 1)
+				 {
+				   cin >> _max_arms;
+				   if (cin.fail())
+				   {
+				   		cin.clear();
+				   		cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				   		cout << "Invalid input: Please re enter: " <<endl;
+				   		continue;
+				   }
+				 flag = 0;
+				 }
+				 flag = 1;
+				 Torso torso(_name,_model_number,_cost,_description,_image_file,_battery_compartments,_max_arms);
 				 break;}
 		case 3 : {cout << "Enter the part name: " << endl;
 				 cin >> _name;
 				 cout << "Enter the model number: " << endl;
-				 cin >> _model_number;
+				 while (flag == 1)
+				 {
+				   cin >> _model_number;
+				   if (cin.fail())
+				   {
+				 	  cin.clear();
+				 	  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				 	  cout << "Invalid input: Please re enter: " << endl;
+				 	  continue;
+				   }
+				 flag = 0;
+				 }
+				 flag =1;
 				 cout << "Enter the cost of the part: " << endl;
-				 cin >> _cost;
+				 while (flag == 1)
+				 {
+				   cin >> _cost;
+				   if (cin.fail())
+				   {
+				 	  cin.clear();
+				 	  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				 	  cout << "Invalid input: Please re enter: " << endl;
+				 	  continue;
+				   }
+				 flag = 0;
+				 }
+				 flag =1;
 				 cout << "Enter the part description: " << endl;
 				 cin >> _description;
-				 cout << "Enter the mage file name: " << endl;
+				 cout << "Enter the image file name: " << endl;
 				 cin >> _image_file;
 				 cout << "Enter the max power of the arm: " << endl;
-				 cin >> _max_power;
-				 Arm stuff3(_name,_model_number,_cost,_description,_image_file,_max_power);
+				 while (flag == 1)
+				 {
+				   cin >> _max_power;
+				   if (cin.fail())
+				   {
+				 	  cin.clear();
+				 	  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				 	  cout << "Invalid input: Please re enter: " << endl;
+				 	  continue;
+				   }
+				 flag = 0;
+				 }
+				 flag =1;
+				 Arm arm(_name,_model_number,_cost,_description,_image_file,_max_power);
 				 break;}
 		case 4 : {cout << "Enter the part name: " << endl;
 				 cin >> _name;
 				 cout << "Enter the model number: " << endl;
-				 cin >> _model_number;
+				 while (flag == 1)
+				 {
+				   cin >> _model_number;
+				   if (cin.fail())
+				   {
+				 	  cin.clear();
+				 	  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				 	  cout << "Invalid input: Please re enter: " << endl;
+				 	  continue;
+				   }
+				 flag = 0;
+				 }
+				 flag =1;
 				 cout << "Enter the cost of the part: " << endl;
-				 cin >> _cost;
+				 while (flag == 1)
+				 {
+				   cin >> _cost;
+				   if (cin.fail())
+				   {
+				 	  cin.clear();
+				 	  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				 	  cout << "Invalid input: Please re enter: " << endl;
+				 	  continue;
+				   }
+				 flag = 0;
+				 }
+				 flag =1;
 				 cout << "Enter the part description: " << endl;
 				 cin >> _description;
-				 cout << "Enter the mage file name: " << endl;
+				 cout << "Enter the image file name: " << endl;
 				 cin >> _image_file;
 				 cout << "Enter the max power of the locomotor: " << endl;
-				 cin >> _max_power;
-				 Locomotor stuff4(_name,_model_number,_cost,_description,_image_file,_max_power);
+				 while (flag == 1)
+				 {
+				   cin >> _max_power;
+				   if (cin.fail())
+				   {
+				 	  cin.clear();
+				 	  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				 	  cout << "Invalid input: Please re enter: " << endl;
+				 	  continue;
+				   }
+				 flag = 0;
+				 }
+				 flag =1;
+				 Locomotor locomotor(_name,_model_number,_cost,_description,_image_file,_max_power);
 				 break;}
 		case 5 : {cout << "Enter the part name: " << endl;
 				 cin >> _name;
 				 cout << "Enter the model number: " << endl;
-				 cin >> _model_number;
+				 while (flag == 1)
+				 {
+				   cin >> _model_number;
+				   if (cin.fail())
+				   {
+				 	  cin.clear();
+				 	  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				 	  cout << "Invalid input: Please re enter: " << endl;
+				 	  continue;
+				   }
+				 flag = 0;
+				 }
+				 flag =1;
 				 cout << "Enter the cost of the part: " << endl;
-				 cin >> _cost;
+				 while (flag == 1)
+				 {
+				   cin >> _cost;
+				   if (cin.fail())
+				   {
+				 	  cin.clear();
+				 	  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				 	  cout << "Invalid input: Please re enter: " << endl;
+				 	  continue;
+				   }
+				 flag = 0;
+				 }
+				 flag =1;
 				 cout << "Enter the part description: " << endl;
 				 cin >> _description;
-				 cout << "Enter the mage file name: " << endl;
+				 cout << "Enter the image file name: " << endl;
 				 cin >> _image_file;
 				 cout << "Enter the power available" << endl;
-				 cin >> _power_available;
-				 cout << "Enter the max enery: " << endl;
-				 cin >> _max_energy;
+				 while (flag == 1)
+				 {
+				   cin >> _power_available;
+				   if (cin.fail())
+				   {
+				 	  cin.clear();
+				 	  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				 	  cout << "Invalid input: Please re enter: " << endl;
+				 	  continue;
+				   }
+				 flag = 0;
+				 }
+				 flag =1;
+				 cout << "Enter the max energy: " << endl;
+				 while (flag == 1)
+				 {
+				   cin >> _max_energy;
+				   if (cin.fail())
+				   {
+				 	  cin.clear();
+				 	  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+				 	  cout << "Invalid input: Please re enter: " << endl;
+				 	  continue;
+				   }
+				 flag = 0;
+				 }
+				 flag =1;
 				 Battery stuff5(_name,_model_number,_cost,_description,_image_file,_power_available,_max_energy);
 				 break;}
 		default : {}
+	}
 }
