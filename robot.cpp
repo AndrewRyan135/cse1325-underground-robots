8c61ca3ad36df28e83145dcbbe41a145a1902c44
warren@smith4.net <warren@smith4.net>
2017-03-27 19:23:58 -0500

    added tostring for robot parts

--- robot.cpp
+++ robot.cpp
@@ -10,26 +10,78 @@ Robot_part :: Robot_part(string _name, int _model_number, double _cost, string _
 Head :: Head(string _name, int _model_number, double _cost, string _description,
             string _image_filename, double _power) : Robot_part(_name, _model_number,
                     _cost, _description, _image_filename), power(_power){}
-//string Head::to_string()
-//{
-
-//}
+string Head::to_string()
+{
+    stringstream os;
+    os << "Name: " << _name << "\n"
+       << "Model Number: " << _model_number << "\n"
+       << "Cost: " << _cost << "\n"
+       << "Description: " << _description << "\n"
+       << "Image Filename: " << _image_filename << "\n"
+       << "Power: " << _power << "\n";
+    return os.str();
+}
 
 Torso :: Torso(string _name, int _model_number, double _cost, string _description,
             string _image_filename, int _battery_compartments, int _max_arms) : Robot_part(_name, _model_number,
                     _cost, _description, _image_filename), battery_compartments(_battery_compartments), max_arms(_max_arms){}
+string Torso::to_string()
+{
+    stringstream os;
+    os << "Name: " << _name << "\n"
+       << "Model Number: " << _model_number << "\n"
+       << "Cost: " << _cost << "\n"
+       << "Description: " << _description << "\n"
+       << "Image Filename: " << _image_filename << "\n"
+       << "Battery Compartments: " << _battery_compartments << "\n";
+    return os.str();
+}
 
 Arm :: Arm(string _name, int _model_number, double _cost, string _description,
             string _image_filename, double _max_power) : Robot_part(_name, _model_number,
                     _cost, _description, _image_filename), max_power(_max_power){}
+string Arm::to_string()
+{
+    stringstream os;
+    os << "Name: " << _name << "\n"
+       << "Model Number: " << _model_number << "\n"
+       << "Cost: " << _cost << "\n"
+       << "Description: " << _description << "\n"
+       << "Image Filename: " << _image_filename << "\n"
+       << "Max Power: " << _max_power << "\n";
+    return os.str();
+}
 
 Locomotor :: Locomotor(string _name, int _model_number, double _cost, string _description,
             string _image_filename, double _max_power) : Robot_part(_name, _model_number,
                     _cost, _description, _image_filename), max_power(_max_power){}
+string Locomotor::to_string()
+{
+    stringstream os;
+    os << "Name: " << _name << "\n"
+       << "Model Number: " << _model_number << "\n"
+       << "Cost: " << _cost << "\n"
+       << "Description: " << _description << "\n"
+       << "Image Filename: " << _image_filename << "\n"
+       << "Max Power: " << _max_power << "\n";
+    return os.str();
+}
 
 Battery :: Battery(string _name, int _model_number, double _cost, string _description,
             string _image_filename, double _power_availible, double _max_energy) : Robot_part(_name, _model_number,
                     _cost, _description, _image_filename), power_availible(_power_availible), max_energy(_max_energy){}
+string Head::to_string()
+{
+    stringstream os;
+    os << "Name: " << _name << "\n"
+       << "Model Number: " << _model_number << "\n"
+       << "Cost: " << _cost << "\n"
+       << "Description: " << _description << "\n"
+       << "Image Filename: " << _image_filename << "\n"
+       << "Power Availible: " << _power_availible << "\n"
+       << "Max Energy: " << _max_energy << "\n";
+    return os.str();
+}
 
 Robot_model :: Robot_model(string _name, int _model_number, Robot_part _torso, 
         Robot_part _head, Robot_part _locomotor, Robot_part _arm, Robot_part _battery) : 
