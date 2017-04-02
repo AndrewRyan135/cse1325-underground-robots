731841380801cf87470b6b913f9945edf11969c9
warren@smith4.net <warren@smith4.net>
2017-04-01 19:20:35 -0500

    working on store.cpp

--- store/store.cpp
+++ store/store.cpp
@@ -1,1 +1,18 @@
+#include "store.h"
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+
+Store :: Store(){}
+
+Store::save(string filename)
+{
+    ofsteam ofs;
+    file.open(filename);
+    ofs.close();
+}
+
 
