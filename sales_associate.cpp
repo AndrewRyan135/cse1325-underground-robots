3ace6de45d4d634c1d2e2f101fab2a3688d55c12
AndrewRyan135 <andrewbreak62@gmail.com>
2017-04-04 00:21:46 -0500

    Master

--- sales_associate.cpp
+++
@@ -1,9 +0,0 @@
-#include "sales_associate.h"
-
-string SalesAssociate::to_string()
-{
-	stringstream os;
-	os << "Sales Associate Info:" << "\n" << "Name: " << _name << "\n"
-	   << "Employee Number: " << _employee_number << "\n";
-	   return os.str();
-}
